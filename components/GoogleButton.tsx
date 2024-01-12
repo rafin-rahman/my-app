deb70298ea3c915f1d4cada7205ea33751f0254e
rafin <ra-fin@live.com>
2024-01-12 15:40:25 +0000

    testing sessions

--- components/GoogleButton.tsx
+++ components/GoogleButton.tsx
@@ -1,32 +1,32 @@
-// "use client";
+"use client";
 import GoogleButton from "react-google-button";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 import { Loader2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-export default async function GoogleComponent() {
-  // const [loading, setLoading] = useState(false);
+export default function GoogleComponent() {
+  const [loading, setLoading] = useState(false);
 
-  async function handleClick() {
-    // setLoading(true);
-    await signIn("google", { redirect: false });
-  }
   return (
     <>
-      {/*{loading ? (*/}
-      <Button className={"mt-14"}>
-        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-        Login in...
-      </Button>
-      {/*) : (*/}
-      <GoogleButton
-        onClick={() => {
-          handleClick();
-        }}
-        className="w-full mt-16"
-      />
-      {/*)}*/}
+      {loading ? (
+        <Button className={"mt-14"}>
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          Login in...
+        </Button>
+      ) : (
+        <GoogleButton
+          onClick={() => {
+            setLoading(true);
+            signIn(
+              "google"
+              // { redirect: false }
+            );
+          }}
+          className="w-full mt-16"
+        />
+      )}
     </>
   );
 }
