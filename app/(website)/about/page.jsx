2a9b0cb3e7c0f4fb52512e71c206d55ad466c486
rafin <ra-fin@live.com>
2024-02-03 10:49:12 +0000

    project name changed to my-app

--- app/(website)/about/page.jsx
+++ app/(website)/about/page.jsx
@@ -4,17 +4,17 @@ import { useSession } from "next-auth/react";
 
 export default function About() {
   const { data: session, status } = useSession();
-  console.log("my client session: ", session);
 
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-          <h1 className="text-6xl font-bold">About</h1>
-          <p className="mt-3 text-2xl">This is the about page.</p>
-          <p>{session.user.id}</p>
-          <p>{session.user.firstName}</p>
-        </main>
+        about
+        {/*<main className="flex flex-col items-center justify-center flex-1 px-20 text-center">*/}
+        {/*  <h1 className="text-6xl font-bold">About</h1>*/}
+        {/*  <p className="mt-3 text-2xl">This is the about page.</p>*/}
+        {/*  <p>{session.user.id}</p>*/}
+        {/*  <p>{session.user.firstName}</p>*/}
+        {/*</main>*/}
       </div>
     </>
   );
